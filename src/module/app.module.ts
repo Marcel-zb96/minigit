67ccc5be81de97308164c2775a8d8721d53eb51d
Marcell Biro <marcell.biro@tappointment.com>
2025-04-04 11:02:04 +0200

    BC-5 implement and configure redis

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,10 +1,27 @@
 import { Logger, MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DataModule } from './data/data.module';
 import { LoggerMiddleware } from '../common/middleware/logger.middleware';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DataModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DataModule,
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        store: await redisStore({
+          host: configService.get<string>('REDIS_HOST'),
+          port: configService.get<string>('REDIS_PORT')!,
+          ttl: configService.get<string>('REDIS_TTL')!,
+        }),
+      }),
+    }),
+  ],
   providers: [Logger],
 })
 export class AppModule implements NestModule {
--- src/module/data/data.controller.ts
+++ src/module/data/data.controller.ts
@@ -1,34 +1,36 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, UseInterceptors } from '@nestjs/common';
 import { DataService } from './data.service';
 import { CreateRepositoryDto, ResponseRepositoryDto } from 'src/schema/repository.schema';
 import { ResponseContributionDto } from 'src/schema/contribution.schema';
 import { ResponseUserDto } from 'src/schema/user.schema';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 
-@Controller('api')
+@UseInterceptors(CacheInterceptor)
+@Controller('api/v1')
 export class DataController {
   constructor(private readonly dataService: DataService) {}
 
-  @Get('/v1/sync')
+  @Get('/sync')
   async syncDb(): Promise<void> {
     await this.dataService.syncDb();
   }
 
-  @Get('/v1/repository')
+  @Get('/repository')
   async getRepositories(@Query('q') q: string): Promise<ResponseRepositoryDto[]> {
     return await this.dataService.getRepositories(q);
   }
 
-  @Get('/v1/repository/:id/contributions')
+  @Get('/repository/:id/contributions')
   async getContributors(@Param('id') id: string): Promise<ResponseContributionDto[]> {
     return await this.dataService.getContributors(id);
   }
 
-  @Get('/v1/user')
+  @Get('/user')
   async getAllUser(): Promise<ResponseUserDto[]> {
     return await this.dataService.getAllUser();
   }
 
-  @Post('v1/repository')
+  @Post('/repository')
   async createRepository(@Body() newRepositoryDto: CreateRepositoryDto): Promise<ResponseRepositoryDto> {
     return await this.dataService.createRepository(newRepositoryDto);
   }
