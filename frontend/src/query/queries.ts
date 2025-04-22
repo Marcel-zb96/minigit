cededd830b7f80f3d3e04fbba40386d9c4762139
Marcell Biro <marcell.biro@tappointment.com>
2025-04-22 11:32:47 +0200

    BC-13 use arrow function in queries file implement fetch wrapper

--- frontend/src/query/queries.ts
+++ frontend/src/query/queries.ts
@@ -2,38 +2,59 @@ import { ContributionResponse } from "@/schema/contribution.schema";
 import { CreateRepository, RepositoryResponse } from "@/schema/repository.schema";
 import { UserResponse } from "@/schema/user.schema";
 
-export async function fetchRepositories(searchString: string): Promise<RepositoryResponse[]> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/repositories?q=${searchString}`)
-    if (!response.ok) {
-        throw new Error(response.statusText)
+export const fetchRepositories = async (searchString: string): Promise<RepositoryResponse[]> => {
+    const queryParam = {
+        url: `repositories?q=${searchString}`,
+        method: "GET",
     }
+    const response = await query(queryParam);
     return await response.json();
 }
 
-export async function fetchUsers(): Promise<UserResponse[]> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/users`);
-    if (!response.ok) {
-        throw new Error(response.statusText);
+export const fetchUsers = async (): Promise<UserResponse[]> => {
+    const queryParam = {
+        url: `users`,
+        method: "GET",
     }
+    const response = await query(queryParam);
     return await response.json();
 }
 
-export async function fechContributions(repositoryId: string): Promise<ContributionResponse[]> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/repositories/${repositoryId}/contributions`);
-    if (!response.ok) {
-        throw new Error(response.statusText);
+export const fechContributions = async (repositoryId: string): Promise<ContributionResponse[]> => {
+    const queryParam = {
+        url: `repositories/${repositoryId}/contributions`,
+        method: "GET",
     }
+    const response = await query(queryParam);
     return await response.json();
 }
 
-export async function createRepository(newRepository: CreateRepository): Promise<RepositoryResponse> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/repositories`, {
+export const createRepository = async (newRepository: CreateRepository): Promise<RepositoryResponse> => {
+    const queryParam = {
+        url: 'repositories',
         method: "POST",
+        body: JSON.stringify(newRepository),
+    }
+    const response = await query(queryParam);
+    return await response.json();
+}
+
+type QueryParams = {
+    url: string;
+    method: string;
+    body?: string,
+};
+
+const query = async ({ url, method, body }: QueryParams): Promise<Response> => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/${url}`, {
+        method,
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(newRepository),
-    })
-    const savedRepository: RepositoryResponse = await response.json();
-    return savedRepository;
+        body,
+    });
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+    return response;
 }
